fa37448e98fc8df1e72ead7f0872d217f26e602a
miriam786 <32094613+miriam786@users.noreply.github.com>
2017-11-12 13:07:34 +0000

    fixed nav bar
    added bootstrap js file

--- nav.js
+++ nav.js
@@ -1,5 +1,8 @@
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-});
+// console.log("here");
+// $('ul.nav li.dropdown').hover(function() {
+// 	console.log("function1");
+//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+// }, function() {
+// 	console.log("function2");
+//   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+// });
